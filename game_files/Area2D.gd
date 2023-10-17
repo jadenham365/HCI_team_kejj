4c4d4591024b4bdf5844041b7ad5152acd9e33b5
Jei <jeichenho@gmail.com>
2023-10-17 04:01:44 -0400

    added win condition

--- game_files/Area2D.gd
+++ game_files/Area2D.gd
@@ -13,9 +13,11 @@ func handlewin():
 
 # restart game once player enters door
 func _on_door_body_entered(body):
-	get_tree().reload_current_scene()
+	if get_tree().get_nodes_in_group("coin_group").size() == 1:
+		get_tree().reload_current_scene()
 
 # TODO: Delete the coin when it is entered by another body
 func _on_coin_body_entered(body):
 	if Input.is_action_pressed("ui_accept"):
 		queue_free()
+		print(get_tree().get_nodes_in_group("coin_group").size())
