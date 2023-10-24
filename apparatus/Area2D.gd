5c3a3c7de47b0348e82fbc84df6e99e3864afa87
Kendrick Xie <kendrickxie@gmail.com>
2023-10-23 20:12:32 -0500

    added windows executable

--- apparatus/Area2D.gd
+++ apparatus/Area2D.gd
@@ -5,14 +5,7 @@ var is_player_inside = false
 var missed_s = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var playernode = get_tree().get_root().find_node("Player", true, false)
-	playernode.connect("win", Global, "handlewin2")
-
-
-# TODO: Make the coins invisible on a win
-func handlewin2():
 	pass
-
 # restart game once player enters door
 func _on_door_body_entered(body):
 	pass
